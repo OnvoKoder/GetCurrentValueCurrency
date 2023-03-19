0f0bb3af451a6e3e25d4bb906e02d04efa5879b5
Daniil Mikhailov <zenitcomreal34@gmail.com>
2023-03-19 20:02:27 +0300

    Add Template

---
+++ Currency/Currency/Class/Manager.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace Currency.Class
+{
+    class Manager
+    {
+        public static Frame MainFrame{ get; set; }
+    }
+}
---
+++ Currency/Currency/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Currency.Class;
+using Currency.Pages;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Currency
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            mainFrame.Navigate(new mainPage());
+            Manager.MainFrame = mainFrame;
+            mainFrame.ContentRendered += (sender, e) => btnBack.Visibility = mainFrame.CanGoBack? Visibility.Visible:Visibility.Hidden;
+            btnBack.Click += (sender,e)=>mainFrame.GoBack();
+        }
+    }
+}
